e3d39295575384e2ff1ffeb9bd1ed1bd54ea02b5
EC2 Default User <ec2-user@ip-172-31-27-197.us-east-2.compute.internal>
2020-05-19 16:01:21 +0000

    added lecture and lab 2

---
+++ lab2.py
@@ -0,0 +1,37 @@
+"""
+Lab 2
+"""
+# 4.1
+my_name = "Tom"
+print(my_name.upper())
+
+# 4.2
+my_id = 123
+print(my_id)
+
+#4.3
+print(type(my_id))
+#no, numbers must be on right side, variables must be on right
+my_id = your_id = 123
+
+
+#4.4
+my_id_str = "123"
+print(my_id_str)
+
+#4.5
+#print(my_name + my_id_str)
+#no
+
+#4.6
+print(my_name + my_id_str)
+
+#4.7
+print(my_name * 3)
+
+#4.8
+print("hello world. this is my first python string.".split("."))
+
+message = "Toms's id is 123"
+print(message)
+
---
+++ lec2.py
@@ -0,0 +1,43 @@
+"""
+this is my second py code
+for my second lecture
+"""
+#print('hello world') #this is a single line comment
+
+"""
+
+This is my second regional comment
+
+"""
+
+# this is my second line comment
+
+#print(type('123'))
+
+#print("Hello World".upper())
+#print("Hello World".lower())
+
+#print("     hello     world     ")
+
+#print("     hello     world     ".strip())
+
+#print("hello, world".split("l"))
+
+#print("hello" + "world"+ ".")
+
+#print(2+3)
+#print(2/3)
+#print(type(print(2/3)))
+
+#print(2**3)
+
+#my_str = "hello world"
+#print(my_str.upper())
+
+#my_str = "Tom"
+#print(my_str)
+
+my_int = 2
+my_float = 3.0
+
+print(my_int + my_float)
