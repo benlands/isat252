edd476d684b30e30d566054a4bf4608d421f7b6a
EC2 Default User <ec2-user@ip-172-31-27-197.us-east-2.compute.internal>
2020-05-20 16:04:05 +0000

    day 3

--- README.md
+++ README.md
@@ -2,3 +2,5 @@
 project for 252
 
 my first python class today.
+
+3rd day
---
+++ lab3.py
@@ -0,0 +1,44 @@
+"""
+lab 3
+"""
+#3.1
+str_list = ['a', 'd', 'e','b','c']
+#print(str_list)
+str_list.sort()
+print(str_list)
+
+#3.2
+str_list.extend('f')
+print(str_list)
+
+#3.3
+str_list.remove('d')
+print(str_list)
+
+#3.4
+print(str_list[2])
+
+#3.5
+my_list = ['a','123',123,'b','B','False',False,123,None,'none']
+
+print(len(set(my_list)))
+
+#3.6
+print(len("This is my third isat lab".split()))
+
+#3.7
+num_list = [12,32,43,35]
+num_list.sort()
+print(num_list)
+
+print(num_list[0]) #min
+print(num_list[-1]) #max
+
+#3.8
+game_board =  [
+              [0,0,0],
+              [0,0,0],
+              [0,0,0]
+                    ]
+game_board[1][1]=1
+print(game_board)
