ab2396a21b218c134f93eb31918f9d63abd66080
EC2 Default User <ec2-user@ip-172-31-27-197.us-east-2.compute.internal>
2020-05-26 16:14:12 +0000

    5th day

---
+++ lab5.py
@@ -0,0 +1,43 @@
+"""
+lab 5
+"""
+#3.1
+
+alien_color = "red"
+
+if alien_color is "green" :
+    print("plus 5 pts")
+    
+#3.2
+if alien_color is "green" :
+    print("plus 5 pts")
+else:
+    print("plus 10 pts")    
+    
+#3.3
+
+favorite_fruits = ["mango","pineapple","persimmon"]
+
+if "strawberry" in favorite_fruits:
+    print("yay, strawberries")
+if "lettuce" in favorite_fruits:
+    print("Not a fruit!")
+if "mango" in favorite_fruits:
+    print("good choice")
+if "pineapple" in favorite_fruits:
+    print("noice!")
+if "persimmon" in favorite_fruits:
+    print("what is that?")
+    
+#3.4
+age = 76
+
+if age <10:
+    print("kid")
+elif age <20 :
+   print("teenager")
+else:
+    print("adult")
+    if age >65:
+        print("elder being")
+        
---
+++ lec5.py
@@ -0,0 +1,44 @@
+"""
+lec5
+"""
+import this
+#print(1     +   4)
+#print([1,2,
+
+#3])
+
+a = [1,2,3]
+b = [1,2,3]
+
+#print(a == b)
+
+#print(a is b)
+
+#print(id(a))
+#print(id(b))
+
+x = None
+
+#print(id(None))
+#print(id(x))
+
+#print(x is None)
+
+#boolean type true/false
+print(True and False) #false becuase of of and operator
+print(True or False) #true
+print(not False) #opposite
+
+
+#if 2>1 :
+   # if 1.5>1:
+   #     print("1.5>1")
+  #  print("2>1")
+
+if 2<=1:
+    print("1")
+elif 2<=2:
+    print("2")
+else:
+    print("3")
+    
